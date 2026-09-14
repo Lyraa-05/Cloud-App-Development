4d7874236cc95795f92e41228ac473faabf9be61
Lyraa-05 <michal05lyra@gmail.com>
2026-09-14 12:08:17 +0100

    Added type-safety to colleague data

--- src/01-basics.ts
+++ src/01-basics.ts
@@ -6,6 +6,15 @@ interface Friend {
     age: number
 }
 
+interface Colleague {
+    name: string;
+    department: string;
+    contact: {
+        email: string;
+        extension: number;
+    }
+}
+
 const friend1 = {
   name: "Michal Lyra",
   phone: "087-12345",
